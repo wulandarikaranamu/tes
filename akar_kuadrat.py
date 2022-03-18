4689fb7d23a3a380d460755df4743fde5dd6319a
Merry Wulandari. S <96329604+wulandarikaranamu@users.noreply.github.com>
2022-03-18 22:36:50 +0800

    komentar

---
+++ akar_kuadrat.py
@@ -0,0 +1,10 @@
+#contoh program menghitung akar kuadrat
+
+# Memasukkan Inputan Angka
+angka = float(input('Tuliskan Angka: '))
+
+# Menghitung Akar Kuadrat
+akar_kuadrat = angka ** 0.5
+
+#Menampilkan Hasil Akar Kuadrat
+print('Akar Kuadrat dari %0.3f adalah %0.3f'%(angka ,akar_kuadrat))
